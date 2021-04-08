ed8dc0ff5a3c0504ff27404cbc06ab20122a174f
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-04-08 10:34:02 +0400

    replaced with formik

--- components/forms/subscribe.js
+++ components/forms/subscribe.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { useFormik } from 'formik';
-import { SUBSCRIBE_ACTION } from '../../constants/path';
-import Input from '../Input';
-import Email from '../Email';
 import Checkbox from '../Checkbox';
 import { SubscribeValidationSchema } from '../../utils/validation';
+import { Formik, Field, Form } from 'formik';
 
 const SubscribeForm = () => {
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            name: ''
-        },
-        validationSchema: SubscribeValidationSchema
-    });
     return (
         <div className="subs-form">
             <div className="subs-form__wrapper">
@@ -22,100 +12,111 @@ const SubscribeForm = () => {
                         <img src="images/hackernoon.png" alt="Hackernoon logo" />
                     </a>
                 </div>
-                <form className="subs-form__form" action={SUBSCRIBE_ACTION} method="POST" acceptCharset="utf-8">
-                    <div className="subs-form__inner">
-                        <p className="subs-form__text">
-                            be the first to read the freshest tech stories from <a className="subs-form__link"
-                                href="http://hackernoon.com/">hackernoon.com</a>
-						by subscribing to <a className="subs-form__link" href="https://www.techbrief.hackernoon.com/">the tech
-							brief</a>:
-						an indiscriminate daily digest from our RSS feed for your reading pleasure and professional
-						development.
-					    </p>
+                <Formik
+                    initialValues={{
+                        name: '',
+                        email: '',
+                        checked: []
+                    }}
+                    validationSchema={SubscribeValidationSchema}
+                    onSubmit={async (values) => {
+                        await new Promise((r) => setTimeout(r, 500));
+                        alert(JSON.stringify(values, null, 2));
+                    }}
+                >
+                    {({ errors, touched, isValidating }) => (
+                        <Form>
+                            <div className="subs-form__inner">
+                                <p className="subs-form__text">
+                                    be the first to read the freshest tech stories from <a className="subs-form__link"
+                                        href="http://hackernoon.com/">hackernoon.com </a>
+                                    by subscribing to <a className="subs-form__link" href="https://www.techbrief.hackernoon.com/">the tech
+                                        brief</a>:
+                                    an indiscriminate daily digest from our RSS feed for your reading pleasure and professional
+                                    development.
+                                </p>
 
-                        <p className="subs-form__text">
-                            oh! and if you let us know which of our editorial streams most floats your boat,
-                            we'll also send you the occasional curated letter from one of the humans on
-                            our team of editors - all of whom are far too busy
-                            publishing stories to spam you in any way, we promise.
-					    </p>
-                    </div>
+                                <p className="subs-form__text">
+                                    oh! and if you let us know which of our editorial streams most floats your boat,
+                                    we'll also send you the occasional curated letter from one of the humans on
+                                    our team of editors - all of whom are far too busy
+                                    publishing stories to spam you in any way, we promise.
+					            </p>
+                            </div>
 
-                    <div className="subs-form__inputs">
-                        <Email
-                            value={formik.values.email}
-                            onBlur={formik.handleBlur}
-                            onChange={formik.handleChange}
-                        />
-                        {formik.touched.email && formik.errors.email ? (
-                            <div className="error-msg" style={{ marginBottom: '10px' }}>{formik.errors.email}</div>
-                        ) : null}
+                            <div className="subs-form__inputs" style={{ marginTop: '30px' }}>
+                                <label className="subs-form__labels" htmlFor='email'>
+                                    <span>Email Address</span>
+                                    <Field
+                                        name="email"
+                                        type="email"
+                                        placeholder="your email"
+                                        required
+                                    />
+                                </label>
+                                {touched.email && errors.email && (
+                                    <div className="error-msg" style={{ marginBottom: '10px' }}>{errors.email}</div>
+                                )}
 
-                        <Input
-                            id="name"
-                            type="text"
-                            name="name"
-                            placeholder="your first name"
-                            htmlFor="name"
-                            spanLabel="First Name"
-                            value={formik.values.name}
-                            onBlur={formik.handleBlur}
-                            onChange={formik.handleChange}
-                        />
-                        {formik.touched.name && formik.errors.name ? (
-                            <div className="error-msg">{formik.errors.name}</div>
-                        ) : null}
-                    </div>
+                                <label className="subs-form__labels" htmlFor='email'>
+                                    <span>First Name</span>
+                                    <Field
+                                        name="name"
+                                        type="text"
+                                        placeholder="your first name"
+                                        required
+                                    />
+                                </label>
+                                {touched.name && errors.name && (
+                                    <div className="error-msg">{errors.name}</div>
+                                )}
+                            </div>
 
-                    <div className="subs-form__checkboxes">
-                        <p>how can hacker noon improve your inbox?</p>
-                        <Checkbox
-                            id="gdpr"
-                            name="gdpr"
-                            text="I concent to receive promotional emails about your products and services"
-                        />
+                            <div className="subs-form__checkboxes">
+                                <p>how can hacker noon improve your inbox?</p>
+                                <div role="group" aria-labelledby="checkbox-group"
+                                    className="subs-form__checkboxes" style={{ padding: '0 0 0 5px', marginBottom: '5px' }}>
+                                    <label className='subs-form__check-label'>
+                                        <Field className="subs-form__checkbox" type="checkbox" name="checked" value="gdpr" />
+                                        I concent to receive promotional emails about your products and services
+                                    </label>
+                                    <label className='subs-form__check-label'>
+                                        <Field className="subs-form__checkbox" type="checkbox" name="checked" value="featured" />
+                                        gimme the techbrief
+                                    </label>
+                                    <label className='subs-form__check-label'>
+                                        <Field className="subs-form__checkbox" type="checkbox" name="checked" value="blockchain&cryptocurrency" />
+                                        do you even blockchain/crypto bruh
+                                    </label>
+                                    <label className='subs-form__check-label'>
+                                        <Field className="subs-form__checkbox" type="checkbox" name="checked" value="programming" />
+                                        I like clean code and I cannot lie
+                                    </label>
+                                    <label className='subs-form__check-label'>
+                                        <Field className="subs-form__checkbox" type="checkbox" name="checked" value="startups&feature&technology" />
+                                        send me stories about startups and the future of everything
+                                    </label>
+                                </div>
+                            </div>
 
-                        <Checkbox
-                            id="featured"
-                            name="bruh"
-                            text="gimme the techbrief"
-                        />
+                            <div style={{ display: 'none' }}>
+                                <label htmlFor="hp">HP</label><br />
+                                <input type="text" name="hp" id="hp" />
+                            </div>
+                            <input type="hidden" name="list" value="OMTKMVEF3BfWHMve4EY8925g" />
+                            <input type="hidden" name="subform" value="yes" />
 
-                        <Checkbox
-                            id="blockchain&cryptocurrency"
-                            name="bruh"
-                            text="do you even blockchain/crypto bruh"
-                        />
-
-                        <Checkbox
-                            id="programming"
-                            name="cleancode"
-                            text="I like clean code and I cannot lie"
-                        />
-
-                        <Checkbox
-                            id="startups&feature&technology"
-                            name="stories"
-                            text="send me stories about startups and the future of
-							everything"
-                        />
-                    </div>
-
-                    <div style={{ display: 'none' }}>
-                        <label htmlFor="hp">HP</label><br />
-                        <input type="text" name="hp" id="hp" />
-                    </div>
-                    <input type="hidden" name="list" value="OMTKMVEF3BfWHMve4EY8925g" />
-                    <input type="hidden" name="subform" value="yes" />
-
-                    <button
-                        id="submit"
-                        type="submit"
-                        name="submit"
-                        className="subs-form__btn"
-                        disabled={formik.errors.name || formik.errors.email}
-                    >Get great stories by email</button>
-                </form>
+                            <button
+                                id="submit"
+                                type="submit"
+                                name="submit"
+                                className="subs-form__btn"
+                                style={{ display: 'block', margin: '0 auto' }}
+                                disabled={errors.name || errors.email}
+                            >Get great stories by email</button>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </div >
     )

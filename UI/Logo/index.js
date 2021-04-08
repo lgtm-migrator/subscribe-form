10ebd14ab30530385c5e6dfc8787511513d4daa7
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-04-08 16:49:06 +0400

    separated logo

---
+++ UI/Logo/index.js
@@ -0,0 +1,9 @@
+const Logo = () => (
+    <div className="subs-form__logo">
+        <a href="https://hackernoon.com">
+            <img src="images/hackernoon.png" alt="Hackernoon logo" />
+        </a>
+    </div>
+)
+
+export default Logo;

0ce3fcfc268f6a25a37d4ad69b32dc1fc6d17ed8
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-04-19 18:26:17 +0400

    package folder updated

---
+++ package/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/style.css';
+
+function App() {
+    return (
+        <>
+            <h1>Hello World From React-Rollups</h1>
+            <img src='../public/favicon.ico' alt='dsdfds' />
+        </>
+    );
+}
+export default App;
